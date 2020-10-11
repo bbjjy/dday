c0f6c29496dea222d82645d02f8030c4a1ed0688
bbjjy <bbjjy1@naver.com>
2020-10-11 21:51:12 +0900

    Updates

---
+++ precache-manifest.b77b4bab8732a1b2c8dbbfca6c64d503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0cf5394bd3f8fe91dc0595307bdafa",
+    "url": "/dday/index.html"
+  },
+  {
+    "revision": "94e15d6d43737547acef",
+    "url": "/dday/static/css/main.210cdb78.chunk.css"
+  },
+  {
+    "revision": "729ddc1bbe044b0a9780",
+    "url": "/dday/static/js/2.cc6adfd2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dday/static/js/2.cc6adfd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e15d6d43737547acef",
+    "url": "/dday/static/js/main.916fbf70.chunk.js"
+  },
+  {
+    "revision": "db5fd407d8d6ae1e050d",
+    "url": "/dday/static/js/runtime-main.d1dfd135.js"
+  },
+  {
+    "revision": "e56f865ba7d4482f5c017bf0a931c25a",
+    "url": "/dday/static/media/alram.e56f865b.mp3"
+  }
+]);
